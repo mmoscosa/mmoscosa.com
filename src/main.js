98c80c2711ee86315b41538259d0bf9817a639a3
Martin Moscosa <mmoscosa@gmail.com>
2019-11-25 02:10:38 +0000

    adding awesome metas

--- src/main.js
+++ src/main.js
@@ -10,4 +10,40 @@ export default function(Vue, { router, head, isClient }) {
   Vue.use(require('vue-moment'))
   var VueTruncate = require('vue-truncate-filter')
   Vue.use(VueTruncate)
+   
+  // Add a meta tag
+  head.meta.push(
+    {
+      name: 'og:title',
+      content: 'Martin Moscosa'
+    },
+    {
+      name: 'og:type',
+      content: 'website'
+    },
+    {
+      name: 'og:image',
+      content: 'https://media.licdn.com/dms/image/C4E03AQHpaBMnDPlMuw/profile-displayphoto-shrink_200_200/0?e=1580342400&v=beta&t=6UUfAk8nRaciP4BRks4CEHMTC4xgaSxTT5-c2TjBSXU'
+    },
+    {
+      name: 'og:url',
+      content: 'https://www.mmoscosa.com'
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary'
+    },
+    {
+      name: 'twitter:site',
+      content: '@mmoscosa'
+    },
+    {
+      name: 'twitter:creator',
+      content: '@mmoscosa'
+    },
+    {
+      name: 'twitter:summary',
+      content: 'https://www.mmoscosa.com'
+    },
+  )
 }
