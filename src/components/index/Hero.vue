0304eecf4accb30a355426e41c2bf0612a89c9df
Martin Moscosa <mmoscosa@gmail.com>
2019-12-04 14:25:14 -0600

    Update Hero.vue

--- src/components/index/Hero.vue
+++ src/components/index/Hero.vue
@@ -13,7 +13,7 @@
 						Martin Moscosa
 					</h1>
 					<p class="leading-normal text-2xl mb-8">
-						Leader in the tech and startup community and product manager
+						Leader in the Tech and Startup Community and Product Manager
 					</p>
 	
 					<button id="call-to-action" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg" v-on:click="seeCV()">
