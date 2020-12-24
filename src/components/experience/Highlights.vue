a5a8fc6edf6286d3361fc511266459c28d3216f8
Martin Moscosa <martin@mmoscosa.com>
2020-12-24 15:48:07 -0600

    :adhesive_bandage: remove section when empty
    removed the section when empty for highlights and recomendations

--- src/components/experience/Highlights.vue
+++ src/components/experience/Highlights.vue
@@ -1,14 +1,33 @@
 <template>
   <section class="bg-gray-100 py-8">
-    <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Highlights</h1>
+    <h1
+      class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800"
+      v-if="highlights.length"
+    >
+      Highlights
+    </h1>
     <div class="container mx-auto flex flex-wrap pt-4 pb-12">
-      <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink clickeable" v-for="highlight in highlights" :key="highlight.id">
-        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+      <div
+        class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink clickeable"
+        v-for="highlight in highlights"
+        :key="highlight.id"
+      >
+        <div
+          class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow"
+        >
           <div class="flex flex-wrap no-underline hover:no-underline">
             <div class="w-full font-bold text-xl text-gray-800 px-6">
-              <g-image class="w-10 pt-2" :src="highlight.logo.file.url" :alt="highlight.title" /> {{highlight.title}}
+              <g-image
+                class="w-10 pt-2"
+                :src="highlight.logo.file.url"
+                :alt="highlight.title"
+              />
+              {{ highlight.title }}
             </div>
-            <p class="text-gray-800 text-base px-6 mb-5" v-html="highlight.description"></p>
+            <p
+              class="text-gray-800 text-base px-6 mb-5"
+              v-html="highlight.description"
+            ></p>
           </div>
         </div>
       </div>
@@ -17,11 +36,9 @@
 </template>
 
 <script>
-  export default {
-    props: ['highlights']
-  }
+export default {
+  props: ['highlights'],
+}
 </script>
 
-<style>
-  
-</style>
+<style></style>
